f11cfadacf1a53886a8dbab02da09b5590d73432
Mussie Tesfay <154098279+mussietesfay@users.noreply.github.com>
2024-09-27 10:06:51 -0700

    Time: 124 ms (70.13%), Space: 70.3 MB (73.22%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if(!head || !head.next) return true;
+    
+    let slow = head;
+    let fast = head;
+    
+    while(fast.next && fast.next.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let curr = slow.next;
+    let pre = null;
+    
+    while(curr){
+        let next = curr.next;
+        curr.next = pre;
+        pre = curr;
+        curr = next;
+    }
+    let p1 = head;
+    let p2 = pre;
+    
+    while(p2){
+        if(p1.val !== p2.val){
+            return false;
+        }else{
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+    }
+    return true;
+};
