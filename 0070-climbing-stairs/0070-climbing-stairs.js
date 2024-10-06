c1c7c35627ab3d0d772e9ec80af39588c3a5c5eb
Mussie Tesfay <154098279+mussietesfay@users.noreply.github.com>
2024-10-06 09:40:50 -0700

    Time: 39 ms (97.41%), Space: 48.8 MB (37.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,17 +2,31 @@
  * @param {number} n
  * @return {number}
  */
-let memo = [];
-var climbStairs = function(n) {
-  if (memo[n] !== undefined) {
-    return memo[n];
-  }
-  if (n === 1) {
-    return 1;
-  }
-  if(n===2){
-    return 2;
-  }
-  memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
-  return memo[n];
+// let memo = [];
+// var climbStairs = function(n) {
+//   if (memo[n] !== undefined) {
+//     return memo[n];
+//   }
+//   if (n === 1) {
+//     return 1;
+//   }
+//   if(n===2){
+//     return 2;
+//   }
+//   memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+//   return memo[n];
+// }
+var climbStairs = function(n){
+    if(n === 0 ) return 0;
+    let memo = new Array (n);
+    
+    memo[0] = 1;
+    memo[1] = 2;
+    
+    for(let i = 2; i<n ; i++){
+        memo[i] = memo[i - 1] + memo[i - 2];
+    }
+    
+    return memo[n - 1]
+    
 }
