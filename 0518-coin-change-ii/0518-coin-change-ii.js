c592930ba34686c56679709edd4524127d823e9a
Mussie Tesfay <154098279+mussietesfay@users.noreply.github.com>
2024-10-09 10:01:57 -0700

    Time: 65 ms (70.59%), Space: 51.5 MB (65.95%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+     let dp=new Array(amount+1).fill(0);
+    dp[0]=1;
+
+        for(let j=0; j<coins.length; j++){
+          for(let i=coins[j]; i<=amount; i++){  
+            dp[i] += dp[i - coins[j]]
+        }
+    }
+    return dp[amount];
+};
