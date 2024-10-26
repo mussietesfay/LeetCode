763b1b2c8d0f03f9143f6702073973cc3a61083f
Mussie Tesfay <154098279+mussietesfay@users.noreply.github.com>
2024-10-26 09:39:46 -0700

    Time: 14 ms (95.57%), Space: 60.3 MB (6.06%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -0,0 +1,103 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    let min = new MinHeap();
+    let dummy = new ListNode(0);
+    let current = dummy;
+    
+    for(let list of lists){
+        if(list){
+        min.insert(list);
+    }
+    }
+    
+    while(min.size() > 0){
+        let removeNode = min.remove();
+        current.next = removeNode;
+        current = current.next;
+        
+        if(removeNode.next){
+          min.insert(removeNode.next)  
+        }
+    }
+    
+    return dummy.next;
+};
+
+class MinHeap{
+    #heap=[];
+
+   getHeap(){
+    return [...this.#heap]
+   } 
+   #left(index){
+    return 2*index + 1;
+   }
+   #rigth(index){
+    return 2*index + 2;
+   }
+   #parent(index){
+    return Math.floor((index - 1)/2);
+   }
+   #swap(index1 , index2){
+    [this.#heap[index1] , this.#heap[index2]]=[this.#heap[index2] , this.#heap[index1]];
+   }
+   peak(){
+    return this.#heap[0];
+   }
+   size(){
+    return this.#heap.length;
+   }
+
+   insert(value){
+    this.#heap.push(value);
+    let current = this.#heap.length - 1;
+    while(current > 0 && this.#heap[current].val < this.#heap[this.#parent(current)].val){
+       this.#swap(current , this.#parent(current)) ;
+       current = this.#parent(current);
+    }
+   }
+    remove(){
+     if(this.#heap.length === 0){
+        return null;
+     }  
+     if(this.#heap.length === 1){
+        return this.#heap.pop();
+     } 
+     let currentNode=this.#heap[0];
+     this.#heap[0] =this.#heap.pop();
+      this.#sinkDown(0)
+      return currentNode;
+    }
+
+    #sinkDown(index){
+   let currentIdex = index;
+   let size = this.#heap.length;
+   while(true){
+    let leftIndex = this.#left(index);
+    let rigthIndex = this.#rigth(index);
+
+    if(leftIndex < size && this.#heap[leftIndex].val < this.#heap[currentIdex].val){
+        currentIdex = leftIndex;
+    }
+    if(rigthIndex < size && this.#heap[rigthIndex].val < this.#heap[currentIdex].val){
+        currentIdex = rigthIndex;
+    }
+    if(currentIdex != index){
+        this.#swap(currentIdex , index);
+        index = currentIdex;
+    }else{
+        return;
+    }
+   }
+    }
+};
