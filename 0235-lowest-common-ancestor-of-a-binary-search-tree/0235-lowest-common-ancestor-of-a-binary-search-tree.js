6834e694df53788a7be297829098f69ec0670655
Mussie Tesfay <154098279+mussietesfay@users.noreply.github.com>
2024-10-29 10:47:15 -0700

    Time: 71 ms (55.02%), Space: 58.7 MB (67.66%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    
+    while(root){
+        
+        // check both p.val and q.val if less than current val
+        if(p.val < root.val && q.val < root.val){
+            root = root.left;
+        }
+        // check both p.val and q.val if  greater than current val
+        else if(p.val > root.val && q.val > root.val){
+            root = root.right;
+        }
+        else{
+            return root; // we found the LCA split val
+        }
+    }
+    return null;
+};
